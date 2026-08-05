c9120911fae29e515cdb6d71af74fb29b4196382
aprecoma <andreaprecoma20@gmail.com>
2026-08-05 20:02:17 +0200

    minor fixes

--- setup/sensors/sensors.sh
+++ setup/sensors/sensors.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-# First argument:   <master_vm_ip>
-# Second argument:  <node_port>
-
-# Enable connection
-
-echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+# First argument:   <master-ip>
+# Second argument:  <broker-port>
 
 # Install Python and MQTT dependencies
-
 apk update
 apk add python3 py3-pip
 pip install paho-mqtt --break-system-packages
 
 # Run the script
-
 python3 sensors.sh $1 $2
--- setup/worker-node/worker-node.sh
+++ setup/worker-node/worker-node.sh
@@ -1,38 +1,22 @@
 #!/bin/bash
-# First argument:   <worker_index>
-# Second argument:  <master_vm_ip>
-# Third argument:   <master_node_token>
+# First argument:   <worker-index>
+# Second argument:  <master-ip>
+# Third argument:   <master-token>
 # 
-# To get <master_node_token>, go to master VM and run
+# To get <master-token>, go to master VM and run
 #   sudo cat /var/lib/rancher/k3s/server/node-token
 
-if [ -z "$1" ]; then
-  echo "Error: Worker ID is required"
-  echo "Usage: $0 <worker_index>"
-  exit 1
-fi
-
-if [ -z "$2" ]; then
-  echo "Error: Master IP is required"
-  echo "Usage: $0 <master_vm_ip>"
-  exit 1
-fi
-
-if [ -z "$3" ]; then
-  echo "Error: Master node token is required"
-  echo "Usage: $0 <master_node_token>"
-  exit 1
-fi
+ZONE="${1:?Usage: $0 <worker-index> <master-ip> <master-token>}"
+MASTER_IP="${2:?Usage: $0 <worker-index> <master-ip> <master-token>}"
+MASTER_TOKEN="${3:?Usage: $0 <worker-index> <master-ip> <master-token>}"
 
 # # Enable connection
 
 # echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 
 # Rename the node (k3s uses hostname as node identity by default)
-
-sudo hostnamectl set-hostname worker-$1
-echo "127.0.1.1 worker-$1" | sudo tee -a /etc/hosts
+sudo hostnamectl set-hostname worker-$ZONE
+echo "127.0.1.1 worker-$ZONE" | sudo tee -a /etc/hosts
 
 # Install k3s in agent mode and join the VM to the cluster
-
-curl -sfL https://get.k3s.io | K3S_URL=https://$2:6443 K3S_TOKEN=$3 sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$MASTER_TOKEN sh -
